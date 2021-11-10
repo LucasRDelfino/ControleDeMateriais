df671716618d10564d6998df4ae7380ce593aa41
ErickAlves7K <erickhue1@hotmail.com>
2021-11-10 16:32:55 -0300

    Primeiros arquivos

---
+++ src/aplicativo/TelaDeInicio.java
@@ -0,0 +1,10 @@
+package aplicativo;
+
+public class TelaDeInicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/aplicativo/TelaDeRemocao.java
@@ -0,0 +1,10 @@
+package aplicativo;
+
+public class TelaDeRemocao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
